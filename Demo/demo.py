7d6dad253efea765cbd6cb37436c77fa82c0823a
Jiachun-Li <65152188+Jiachun-Li@users.noreply.github.com>
2020-06-04 16:28:33 +0800

    Update demo.py

--- Demo/demo.py
+++ Demo/demo.py
@@ -10,15 +10,15 @@ angry：皱眉眼睛大Y
 fear：皱眉眼睛大张嘴y
 sad：皱眉，眼睛小
 happy：嘴宽度大y#
-
 """
 
 
-
+def puttext(emotion, cartoon, name):
+    cv2.putText(cartoon, "hello", (d.left(), d.bottom() + 30), font, 1, (0, 0, 0), 2)
+    cv2.imwrite(str(name), cartoon)
 
 
 def cartoonise(emotion, picture_name, num):
-
     num_bilateral = 7  # 定义双边滤波的数目
 
     img_rgb = cv2.imread(picture_name)
@@ -44,11 +44,10 @@ def cartoonise(emotion, picture_name, num):
                                      cv2.THRESH_BINARY,
                                      blockSize=5, C=7)
     img_edge = cv2.cvtColor(img_edge, cv2.COLOR_GRAY2RGB)
-    #img_cartoon = cv2.bitwise_and(img_color, img_edge)
-    img_cartoon = cv2.addWeighted(img_color,0.8, img_edge,0.2,0)
+    # img_cartoon = cv2.bitwise_and(img_color, img_edge)
+    img_cartoon = cv2.addWeighted(img_color, 0.8, img_edge, 0.2, 0)
     name = 'cartoon/' + str(emotion) + str(num) + '.jpg'
-    cv2.imwrite(name, img_cartoon)
-
+    puttext(emotion, img_cartoon, name)
 
 
 # 使用特征提取器 get_frontal_face_detector
@@ -57,8 +56,6 @@ detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 # 建cv2摄像头对象，参数0表示打开电脑自带的摄像头，如果换了外部摄像头，则自动切换到外部摄像头
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-# set(propId, value),设置视频参数，propId设置视频参数， value设置参数值
-#cap.set(3, 480)
 # 截取 screenshoot 的计数器
 happy, disgust, fear, sad, angry, surprise, nature = 1, 1, 1, 1, 1, 1, 1
 
@@ -76,7 +73,8 @@ while cap.isOpened():
     font = cv2.FONT_ITALIC
     # 复制，用于照相
     draw_img = frame.copy()
-
+    cv2.putText(frame, "Press 'q' to take a picture", (10, 20), font, 0.6, (0, 0, 0), 2)
+    cv2.putText(frame, "Press 'Esc' to end", (10, 50), font, 0.6, (0, 0, 0), 2)
     # 检测到人脸 / Face detected
     if len(faces) != 0:
         # 矩形框 / Show the rectangle box of face
@@ -101,32 +99,32 @@ while cap.isOpened():
                 # radius,Radius of the circle (半径)
                 # color，Circle color
                 cv2.circle(frame, (shape.part(i).x, shape.part(i).y), 2, (0, 255, 0), 1)
-                #cv2.putText(frame, str(i), (shape.part(i).x, shape.part(i).y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
+                # cv2.putText(frame, str(i), (shape.part(i).x, shape.part(i).y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,
+                # 255, 255))
             # 分析任意 n 点的位置关系来作为表情识别的依据
             # 嘴中心	66，嘴左角48，嘴右角54
-            mouth_width = (shape.part(54).x - shape.part(48).x) / face_width  # 嘴巴张开程度
-            mouth_height = (shape.part(66).y - shape.part(62).y) / face_height # 嘴巴张开程度
-            # print("嘴巴宽度与识别框宽度之比：" , mouth_width)
-            # print("嘴巴高度与识别框宽度之比：" , mouth_height)
-            eye_height = (shape.part(41).y - shape.part(37).y) / face_height
-            #print("eye", eye_height)
+            mouth_width = (shape.part(54).x - shape.part(48).x) / face_width  # 嘴巴张开宽度
+            mouth_height = (shape.part(66).y - shape.part(62).y) / face_height  # 嘴巴张开高度
+            eye_height = (shape.part(41).y - shape.part(37).y) / face_height  # 眼睛睁开程度
             frown_sum = 0  # 两边眉毛距离之和
             for j in range(17, 21):
                 frown_sum += shape.part(j + 5).x - shape.part(j).x
             frown_sum = frown_sum / face_width
 
-
             if mouth_width >= 0.34:
                 if frown_sum <= 1.8:
-                    cv2.putText(frame, "disgust", (d.left(), d.bottom() + 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, 4)
+                    cv2.putText(frame, "disgust", (d.left(), d.bottom() + 20), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                                (0, 0, 255), 2, 4)
                     if kk == ord('q'):
                         name = 'disgust/picture' + str(disgust) + '.jpg'
                         cv2.imwrite(name, draw_img)
                         cartoonise("disgust", name, disgust)
                         print("disgust", disgust)
                         disgust += 1
+
                 else:
-                    cv2.putText(frame, "happy", (d.left(), d.bottom() + 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, 4)
+                    cv2.putText(frame, "happy", (d.left(), d.bottom() + 20), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                                (0, 0, 255), 2, 4)
                     if kk == ord('q'):
                         name = 'happy/picture' + str(happy) + '.jpg'
                         cv2.imwrite(name, draw_img)
@@ -134,10 +132,10 @@ while cap.isOpened():
                         print("happy", happy)
                         happy += 1
 
-
             elif mouth_height >= 0.05:
                 if frown_sum <= 1.8:
-                    cv2.putText(frame, "fear", (d.left(), d.bottom() + 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, 4)
+                    cv2.putText(frame, "fear", (d.left(), d.bottom() + 20), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                                (0, 0, 255), 2, 4)
                     if kk == ord('q'):
                         name = 'fear/picture' + str(fear) + '.jpg'
                         cv2.imwrite(name, draw_img)
@@ -145,7 +143,8 @@ while cap.isOpened():
                         print("fear", fear)
                         fear += 1
                 else:
-                    cv2.putText(frame, "surprise", (d.left(), d.bottom() + 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, 4)
+                    cv2.putText(frame, "surprise", (d.left(), d.bottom() + 20), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                                (0, 0, 255), 2, 4)
                     if kk == ord('q'):
                         name = 'surprise/picture' + str(surprise) + '.jpg'
                         cv2.imwrite(name, draw_img)
@@ -155,15 +154,17 @@ while cap.isOpened():
 
             elif mouth_width <= 0.26:
                 if eye_height < 0.03:
-                    cv2.putText(frame, "sad", (d.left(), d.bottom() + 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, 4)
+                    cv2.putText(frame, "sad", (d.left(), d.bottom() + 20), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                                (0, 0, 255), 2, 4)
                     if kk == ord('q'):
                         name = 'sad/picture' + str(sad) + '.jpg'
                         cv2.imwrite(name, draw_img)
                         cartoonise("sad", name, sad)
-                        print("sady", sad)
+                        print("sad", sad)
                         sad += 1
                 else:
-                    cv2.putText(frame, "angry", (d.left(), d.bottom() + 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, 4)
+                    cv2.putText(frame, "angry", (d.left(), d.bottom() + 20), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                                (0, 0, 255), 2, 4)
                     if kk == ord('q'):
                         name = 'angry/picture' + str(angry) + '.jpg'
                         cv2.imwrite(name, draw_img)
@@ -171,16 +172,14 @@ while cap.isOpened():
                         print("angry", angry)
                         angry += 1
             else:
-                cv2.putText(frame, "nature", (d.left(), d.bottom() + 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, 4)
+                cv2.putText(frame, "nature", (d.left(), d.bottom() + 20), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                            (0, 0, 255), 2, 4)
                 if kk == ord('q'):
                     name = 'nature/picture' + str(nature) + '.jpg'
                     cv2.imwrite(name, draw_img)
                     cartoonise("nature", name, nature)
                     print("nature", nature)
                     nature += 1
-
-
-
 
     else:
         # 没有检测到人脸
@@ -190,7 +189,6 @@ while cap.isOpened():
     if kk == 27:
         print('goodbye')
         break
-
 
     cv2.imshow("emotion", frame)
 
