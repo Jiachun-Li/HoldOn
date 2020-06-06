bb02758ae49674eb1608e6ad1ea43f20b29b6f7d
Jiachun-Li <65152188+Jiachun-Li@users.noreply.github.com>
2020-06-06 21:33:24 +0800

    Update demo.py

--- Demo/demo.py
+++ Demo/demo.py
@@ -1,29 +1,37 @@
-import cv2  # 图像处理的库 OpenCv
-import dlib  # 人脸识别的库 dlib
+import cv2  
+import dlib  
+import random
 
-
-# 开心 惊讶 正常 愤怒 悲伤 恐惧 厌恶
+# 开心 惊讶 自然 愤怒 悲伤 恐惧 厌恶
 """
-surprise：不皱眉张嘴y
-disgust：咧嘴皱眉Y
-angry：皱眉眼睛大Y
-fear：皱眉眼睛大张嘴y
+surprise：不皱眉张嘴
+disgust：咧嘴皱眉
+angry：皱眉眼睛大
+fear：皱眉眼睛大张嘴
 sad：皱眉，眼睛小
-happy：嘴宽度大y#
+happy：嘴宽度大
 """
-def screenshoot(emotion, num):
-    name = f'{emotion}/picture{num}' + '.jpg'
-    cv2.imwrite(name, draw_img)
-    cartoonise(str(emotion), name, num)
-    print(f"{emotion},{num}")
+
+
 
 def puttext(emotion, cartoon, name):
-    cv2.putText(cartoon, "hello", (d.left(), d.bottom() + 30), font, 1, (0, 0, 0), 2)
+    happy_word = ["hahahaha", "Today is a good day!", "Aha"]
+    angry_word = ["piss me off", "Don't bother me!"]
+    disgust_word = ["disgusting", "hate you", "stay away from me", "Ugh"]
+    sad_word = ["Life is so hard.", "TAT", "feel blue now..."]
+    fear_word = ["It's' horrible!", "That sounds awful."]
+    surprise_word = ["What a surprise!", "This shocked me", "Wow!", "Awesome", "can't believe it"]
+    nature_word = ["just so so", "emmmm"]
+    emotion_list = [happy_word, angry_word, disgust_word, sad_word, fear_word, surprise_word, nature_word]
+    choice = ["happy", "angry", "disgust", "sad", "fear", "surprise", "nature"]
+    location = choice.index(str(emotion))
+    cv2.putText(cartoon, random.choice(emotion_list[location]), (d.left(), d.bottom() + 50), font, 1,
+                (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), 2)
     cv2.imwrite(str(name), cartoon)
 
 
 def cartoonise(emotion, picture_name, num):
-    num_bilateral = 7  # 定义双边滤波的数目
+    num_bilateral = 10  # 定义双边滤波的数目
 
     img_rgb = cv2.imread(picture_name)
 
@@ -33,7 +41,7 @@ def cartoonise(emotion, picture_name, num):
     img_color = cv2.pyrDown(img_color)
 
     # 重复使用小的双边滤波代替一个大的滤波
-    for _ in range(10):
+    for _ in range(num_bilateral):
         img_color = cv2.bilateralFilter(img_color, d=9,
                                         sigmaColor=9,
                                         sigmaSpace=7)
@@ -43,15 +51,25 @@ def cartoonise(emotion, picture_name, num):
     # 转换为灰度并使其产生中等的模糊
     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
     img_blur = cv2.medianBlur(img_gray, 3)
+    # 使用自适应阈值处理灰度图像创建轮廓
     img_edge = cv2.adaptiveThreshold(img_blur, 255,
                                      cv2.ADAPTIVE_THRESH_MEAN_C,
                                      cv2.THRESH_BINARY,
                                      blockSize=5, C=7)
+    # 转换回色彩图像
     img_edge = cv2.cvtColor(img_edge, cv2.COLOR_GRAY2RGB)
-    # img_cartoon = cv2.bitwise_and(img_color, img_edge)
+    # 叠加彩色图像和轮廓
     img_cartoon = cv2.addWeighted(img_color, 0.8, img_edge, 0.2, 0)
+    # 命名并保存图像
     name = 'cartoon/' + str(emotion) + str(num) + '.jpg'
     puttext(emotion, img_cartoon, name)
+
+
+def screenshoot(emotion, num):
+    name = f'{emotion}/picture{num}' + '.jpg'
+    cv2.imwrite(name, draw_img)
+    cartoonise(str(emotion), name, num)
+    print(f"{emotion},{num}")
 
 
 # 使用特征提取器 get_frontal_face_detector
@@ -77,14 +95,13 @@ while cap.isOpened():
     font = cv2.FONT_ITALIC
     # 复制，用于照相
     draw_img = frame.copy()
-    cv2.putText(frame, "Press 'q' to take a picture", (10, 20), font, 0.6, (0, 0, 0), 2)
-    cv2.putText(frame, "Press 'Esc' to end", (10, 50), font, 0.6, (0, 0, 0), 2)
+    cv2.putText(frame, "Press 'q' to take a picture", (10, 20), font, 0.6, (0, 200, 0), 2)
+    cv2.putText(frame, "Press 'Esc' to end", (10, 50), font, 0.6, (0, 200, 0), 2)
     # 检测到人脸 / Face detected
     if len(faces) != 0:
         # 矩形框 / Show the rectangle box of face
         for k, d in enumerate(faces):
             # 计算矩形大小
-            # Compute the width and height of the box
             # (x,y), (宽度width, 高度height)
             pos_start = tuple([d.left(), d.top()])
             pos_end = tuple([d.right(), d.bottom()])
@@ -92,19 +109,13 @@ while cap.isOpened():
             # 计算矩形框大小 / compute the size of rectangle box
             face_height = (d.bottom() - d.top())
             face_width = (d.right() - d.left())
-            # 用红色矩阵框出人脸, 光的三原色Red(0,0,255), Green(0,255,0), Blue(255,0,0)
-            #  rectangle(img, pt1, pt2, color), 其中pt1为矩阵上顶点，pt2为矩阵下顶点
+            # 用黄色矩阵框出人脸, 光的三原色Red(0,0,255), Green(0,255,0), Blue(255,0,0)
+            #  rectangle(img, pt1, pt2, color)
             cv2.rectangle(frame, pos_start, pos_end, (0, 255, 255), 2)
             shape = predictor(frame, d)
             for i in range(68):
                 # circle(img, center, radius, color),
-                # img,Image where the circle is drawn
-                # center,Center of the circle
-                # radius,Radius of the circle (半径)
-                # color，Circle color
                 cv2.circle(frame, (shape.part(i).x, shape.part(i).y), 2, (0, 255, 0), 1)
-                # cv2.putText(frame, str(i), (shape.part(i).x, shape.part(i).y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,
-                # 255, 255))
             # 分析任意 n 点的位置关系来作为表情识别的依据
             # 嘴中心	66，嘴左角48，嘴右角54
             mouth_width = (shape.part(54).x - shape.part(48).x) / face_width  # 嘴巴张开宽度
@@ -168,14 +179,15 @@ while cap.isOpened():
         # 没有检测到人脸
         cv2.putText(frame, "No Face", (20, 50), font, 0.6, (0, 0, 255), 1, cv2.LINE_AA)
 
-    # 6. 按下 'q' 键退出 / Press 'q' to exit
+    # 按下 'Esc' 键退出
     if kk == 27:
         print('goodbye')
         break
 
     cv2.imshow("emotion", frame)
 
-# 释放摄像头 / Release camera and destroy all windows
+# 释放摄像头
 cap.release()
 cv2.destroyAllWindows()
+
 
